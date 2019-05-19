6a7493c0cd44f9d9fe8c29895eb28d681938b53a
Michelle Lam <shelly2.lam@gmail.com>
2019-05-19 00:48:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {healthy: "salad", meat:"steak", fish: "salmon"}
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  recipes[key]=value
+  recipes[key] = value
   return recipes
 }
 
