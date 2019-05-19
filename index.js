a532d35bfafc5933da785197e451b267b898bef6
Michelle Lam <shelly2.lam@gmail.com>
2019-05-19 00:45:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  recipes[key] = 'value'
+  recipes[key] = value
   return recipes
 }
 
