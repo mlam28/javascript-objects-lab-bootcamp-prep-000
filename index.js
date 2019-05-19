62734a96549d6a3a84c1155a77ecc2af02b035f5
Michelle Lam <shelly2.lam@gmail.com>
2019-05-19 01:13:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({}, object, {[key]:value})
+  delete newObj[key]
+  return newObj
+}
