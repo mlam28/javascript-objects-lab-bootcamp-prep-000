93c33e8b7358a516f50d8f8946a52c194b83d0f9
Michelle Lam <shelly2.lam@gmail.com>
2019-05-19 00:42:26 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+var recipes = {healthy: "salad", meat:"steak", fish: "salmon"}
+
+function updateObjectWithKeyAndValue(object, key, value,) {
+  recipes[key]=value
+  return recipes
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  recipes[key] = 'value'
+  }
+  return recipes
+}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return recipes.assign({}, object, {[key]:value})
+  return recipes
+}
